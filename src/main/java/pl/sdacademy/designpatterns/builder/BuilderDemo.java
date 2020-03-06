1d58ff6839bd335b79a84041eb1293e91c663e7e
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-03-06 11:38:28 +0100

    initial commit

--- src/main/java/pl/sdacademy/designpatterns/builder/BuilderDemo.java
+++ src/main/java/pl/sdacademy/designpatterns/builder/BuilderDemo.java
@@ -3,6 +3,7 @@ package pl.sdacademy.designpatterns.builder;
 import java.util.List;
 
 public class BuilderDemo {
+
     public static void main(String[] args) {
       final Player player = new Player.Builder().withHealth(100)
                 .withMana(50)
